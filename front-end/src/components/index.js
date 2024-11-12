af0574a887cc79297da0e013ba40f73cb9c4910e
V <2355228@cognizant.com>
2024-11-12 20:04:06 +0530

    test

---
+++ front-end/src/components/index.js
@@ -0,0 +1,49 @@
+import Navigation from "./Navigation";
+import Home from "./Home";
+import Manage from "./Manage";
+import NotFound from "./NotFound";
+import Execute from "./Execute";
+import TestsGrid from "./TestsGrid";
+import JobMonitor from "./JobMonitor";
+import ViewJob from "./ViewJob";
+import JobSubmission from "./JobSubmission";
+import User from "./User";
+import AddOrUdpate from "./AddOrUpdate";
+import SideNav from "./SideNav";
+import ManageMapping from "./ManageMapping";
+import EDIEXtracts from "./EDIExtracts";
+import OnlineClaimTrigger from "./OnlineClaimTrigger";
+import Dashboard from "./Dashboard";
+import ClaimValidationResult from "./ClaimValidationResult";
+import CVQueueAndForm from "./CVQueueAndForm";
+import ClaimAccuracyValidator from "./ClaimAccuracyValidator";
+import ClaimAccuracyConditions from "./ClaimAccuracyConditions";
+import ClaimAccuracyResult from "./ClaimAccuracyResult";
+import CreateUser from "./CreateUser";
+import Testcases from "./Testcases";
+
+export {
+  Navigation,
+  SideNav,
+  User,
+  Home,
+  Manage,
+  NotFound,
+  AddOrUdpate,
+  Execute,
+  TestsGrid,
+  JobSubmission,
+  JobMonitor,
+  ViewJob,
+  ManageMapping,
+  EDIEXtracts,
+  OnlineClaimTrigger,
+  Dashboard,
+  ClaimValidationResult,
+  CVQueueAndForm,
+  ClaimAccuracyValidator,
+  ClaimAccuracyConditions,
+  ClaimAccuracyResult,
+  CreateUser,
+  Testcases,
+};
